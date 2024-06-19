9083e1841cf5b98880d16ba2181f46b0f32fdb8c
akbaroke <akbarmuhammad833@gmail.com>
2024-06-19 22:51:58 +0700

    fix env

--- backend/src/application/web.ts
+++ backend/src/application/web.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { publicRouter } from '../route/public-api';
 import { errorMiddleware } from '../middleware/error-middleware';
 import apiRouter from '../route/api';
+import '../config/env-config';
 
 export const web = express();
 web.use(express.json());
+web.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  })
+);
 web.use(publicRouter);
 web.use(apiRouter);
 web.use(errorMiddleware);
