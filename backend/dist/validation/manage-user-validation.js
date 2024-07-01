141b42e415ed0f38a2703aabd98b8026e7a9bda1
akbaroke <akbarmuhammad833@gmail.com>
2024-07-01 11:33:46 +0700

    update config build

--- backend/dist/validation/manage-user-validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ManageUserValidation = void 0;
-const zod_1 = require("zod");
-exports.ManageUserValidation = zod_1.z.object({
-    is_banned: zod_1.z.boolean().optional(),
-    role: zod_1.z.enum(['ADMIN', 'STUDENT', 'TEACHER']).optional(),
-    name: zod_1.z.string().min(1).max(255).optional(),
-});
--- backend/dist/validation/validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Validation = void 0;
-class Validation {
-    static validate(schema, data) {
-        return schema.parse(data);
-    }
-}
-exports.Validation = Validation;
